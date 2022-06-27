91a4ba096aaf5bdba1377f32ebd7a60003f3cd7c
Agung Firmansyah <agungfirid@gmail.com>
2022-06-27 17:14:22 +0700

    fix

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: "AIzaSyBHVboP51aB6JHVlIZFflRyBFRoyo8tPI0",
-  authDomain: "monitoring-simplisia.firebaseapp.com",
-  projectId: "monitoring-simplisia",
-  storageBucket: "monitoring-simplisia.appspot.com",
-  messagingSenderId: "100591674235",
-  appId: "1:100591674235:web:acc43c14c271a33e3953de",
-  measurementId: "G-C9FPP2JLYZ"
+  apiKey: 'AIzaSyBHVboP51aB6JHVlIZFflRyBFRoyo8tPI0',
+  authDomain: 'monitoring-simplisia.firebaseapp.com',
+  projectId: 'monitoring-simplisia',
+  storageBucket: 'monitoring-simplisia.appspot.com',
+  messagingSenderId: '100591674235',
+  appId: '1:100591674235:web:acc43c14c271a33e3953de',
+  measurementId: 'G-C9FPP2JLYZ',
 };
 
 module.exports = config;
